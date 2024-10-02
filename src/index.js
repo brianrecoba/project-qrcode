ae2bec5d62da46141a7a8e6d61a472b0e814233d
Brian Recoba Fernandes <brian.fernandes@getnet.com.br>
2024-10-02 19:41:40 -0300

    Feature: Add the chalk package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import prompt from "prompt";
+import mainPrompt from "../src/prompts/prompt-main.js"
 
 
 async function main() {
-
+    prompt.get(mainPrompt, async (err, choose) => {
+        if (choose.select == 1) console.log("Você escolheu o QRCode");
+        if (choose.select == 2) console.log("Você escolheu o Passwords");
+    })
+    prompt.start();
 }
 
 main();
